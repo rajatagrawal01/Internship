160e1308841aed5a389503a5ec63dd6bfddbf4ec
rajatagrawal01 <rajatagrawal01@gmail.com>
2025-06-24 14:39:39 +0530

    console-line

---
+++ JS/line_in_console.js
@@ -0,0 +1,14 @@
+
+for(var i=0;i<=5;i++){
+    var line = "";
+    for(var j=0;j<i;j++){
+        line += "*";
+    }
+    console.log(line);
+}
+
+// var line = "";
+// for(var i=0;i<=5;i++){
+//     line+=i;
+// }
+// console.log(line);

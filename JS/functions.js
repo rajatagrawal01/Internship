6b23431f79830fe7f776933b855c0628326a4ee4
rajatagrawal01 <rajatagrawal01@gmail.com>
2025-06-25 14:33:40 +0530

    functions

---
+++ JS/functions.js
@@ -0,0 +1,63 @@
+// Collection of oerations or set of operations to be performed via single click
+
+// Funtions==methods
+// Usage:
+// 1- Reusablility
+// 2- Readability
+// 3- Maintainability
+// 4- Code Shrinking
+// 5- Debuging
+// 6- Performance
+
+// Types of Functions:
+
+// 1- Named Function
+// function printHello(){
+//     console.log("Hello");
+// }
+
+// printHello()
+
+// 2 - 
+// params,parameters,arguments
+// function add(a,b){
+//     var c= a + b;
+//     console.log(c);
+// }
+
+// add(2,3)
+
+// return value
+
+// function sub(a,b) {
+//     return a - b;
+// }
+// console.log("Direct print",sub(5,2));
+
+// var result = sub(5,2);
+// console.log("Variabl function",result);
+
+// function maths(a,b){
+//     console.log("Exponentiation:", a ** b);
+//     console.log("Subtraction:", a - b);
+//     console.log("Multiplication:", a * b);
+//     console.log("Division:", a / b);
+//     console.log("Modulus:", a % b);
+//     console.log("Addition:", a + b);
+// }
+
+// maths(10,5)
+// maths(11,6)
+// maths(12,7)
+// maths(13,8)
+// maths(14,9)
+
+
+// Arrow Function
+
+// var add = (a,b) => {
+//     return a + b;
+// }
+
+// console.log(add(3,4));
+

15b93955ddde97ad3b3adeb31d9cae0ee365162d
rajatagrawal01 <rajatagrawal01@gmail.com>
2025-07-02 14:29:17 +0530

    array

---
+++ JS/array.js
@@ -0,0 +1,45 @@
+// Array is a set of values
+// Indexing
+
+// var arr = [2,3,5,6,'Hello',true];
+// 1D array
+var arr = [1,2,3,4,5];
+var arr2 = ['H','e','l','l','o'];
+
+// 2D array
+// var arr =
+// [
+//     [1,2,3]
+// ]
+
+// 3D array
+// var arr =
+// [
+//     [
+//         [1,2,3]
+//     ]
+// ]
+
+// var lastValue=arr.pop()
+// console.log(lastValue);
+// arr.pop(); Removing value from last
+// arr.push(6); Adding value in last
+// arr.unshift(0); Adding value from first
+// arr.shift(); Removing value from first
+// var arr3 = arr.concat([1,2,3,4,5])
+// var arr4 = arr2.concat(['d','e','f']);
+// arr2.join() Defines how array will print
+// arr.indexOf(3) Provides index of an element
+// arr.includes(3) Provides availability of a value (return boolen)
+// arr.reverse() Prints reverse of an array
+// arr.every()
+// var double =arr.map(x=> x*2);
+// console.log(double);
+// arr.every(x=>x>0)
+// console.log(arr.every(x=>x>0));
+
+console.log(arr[0][0]);
+
+
+
+
